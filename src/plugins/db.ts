acd89357afe4e1dbc388ef13f5c3a9697a358155
JPCabral04 <jpqcabral04@gmail.com>
2025-02-19 16:41:48 -0300

    Integrate MongoDB with server and update .gitignore. Closes #2,Closes #3

---
+++ src/plugins/db.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI as string);
+    console.log("🔥 Conectado ao MongoDB");
+  } catch (error) {
+    console.error("❌ Erro ao conectar ao MongoDB:", error);
+    process.exit(1);
+  }
+}
